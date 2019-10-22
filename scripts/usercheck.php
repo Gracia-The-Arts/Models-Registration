550019dd32fa17a4789dc58d720fb1352446d5d3
Benson Imoh,ST <benson@stbensonimoh.com>
2019-10-22 13:14:08 +0100

    Fix minor issues

--- scripts/usercheck.php
+++ scripts/usercheck.php
@@ -31,7 +31,7 @@ if ($db->userExists($email, "modelsreg")) {
         foreach ($result as $key => $value) {
             ${$key} = $value;
         }
-        $amount = 25000;
+        $amount = 2500;
 
         // Run CURL
         $curl = curl_init();
